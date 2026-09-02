5abb550289539d2c4461fcd49bdcd765c2d78875
cadmium2525 <cadmium2525@gmail.com>
2026-09-02 16:25:37 +0900

    Redesign arena opponent selection

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'monster-construction-';
-const CACHE_VERSION = '1.26.2';
+const CACHE_VERSION = '1.27.0';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const SCOPE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('./index.html', SCOPE_URL).toString();
